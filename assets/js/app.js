50c517e582559147c0e36a52c4c8903318c18f36
Jake Toolson <jaketoolson@gmail.com>
2013-07-07 21:41:31 -0700

    globals bugging me

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,50 @@
-        // OMG global vars. Whatevs... WIP nuggz
-        var deviceReadyDeferred = $.Deferred(),
-            jqmReadyDeferred = $.Deferred(),
-            toPageId = null,
 
-            app = {
+            // WIP nuggz... 
+            
+            var app = {
+
+                deviceReadyDeferred : $.Deferred(),
+                jqmReadyDeferred : $.Deferred(),
+                toPageId : null,
 
                 init: function(){                  
                     this.bindEvents();
                 },
 
                 bindEvents: function(){
-                    document.addEventListener("deviceReady", this.onDeviceReady, false);
+                    document.addEventListener('deviceReady', this.onDeviceReady, false);
                     $(document).bind('pagechange', this.onPageChange);
-                    $(document).one("mobileinit", this.onMobileInit);
+                    $( window ).on('orientationchange', this.onOrientationChange);
+                    $(document).one('mobileinit', this.onMobileInit);
                     console.log('events bound');
                 },
 
                 onDeviceReady: function(){
                     console.log('device readyd!');
-                    deviceReadyDeferred.resolve();
+                    app.deviceReadyDeferred.resolve();
                 },
 
                 onMobileInit: function(){
                     console.log('mobile initd!');
-                    jqmReadyDeferred.resolve();
+                    app.jqmReadyDeferred.resolve();
                     $.extend(  $.mobile , {
                         defaultPageTransition : 'slide'
                     });
                 },
 
                 onPageChange: function(e,data){
-                    toPageId = data.toPage.attr("id");
-                    console.log('Page: '+toPageId+' opened!');
-                    $('#'+toPageId+' .iosSlider').iosSlider({
+                    app.toPageId = data.toPage.attr('id');
+                    console.log('Page: '+app.toPageId+' opened!');
+                    app.initIosSlider(app.toPageId);
+                },
+
+                onOrientationChange: function(e){
+                    console.log('Orientation '+e.orientation);
+                    app.initIosSlider(this.toPageId);
+                },
+
+                initIosSlider: function(pageId){
+                    $('#'+pageId+' .iosSlider').iosSlider({
                         snapToChildren: true,
                         desktopClickDrag: true
                     });
